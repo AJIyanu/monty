3c663f314e4adcf53fed0e2cc13e9a759f9c5b99
AJIyanu <aderemijoseph@live.com>
2022-06-19 05:48:33 +0100

    functions written

--- opcode3.c
+++ opcode3.c
@@ -57,9 +57,141 @@ void div(stack_t **stack, unsigned int line_number)
  * Return: void
  */
 
-void mul(stack_t **stack, unsigned int line_number)
+void _mul(stack_t **stack, unsigned int line_number)
 {
 	stack_t *ptr = *stack;
 	int muled;
 
 	if (counter(stack) < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		cleanStack(stack);
+		exit(EXIT_FAILURE);
+	}
+	if (mode == 1)
+	{
+		while (ptr->n != NULL)
+			ptr = ptr->next;
+		ptr = ptr->prev;
+		muled = (ptr->n) * ((ptr->next)->n);
+		ptr->n = muled;
+		_pop(stack, line_number);
+	}
+	if (mode == 2)
+	{
+		ptr = ptr->next;
+		muled = (ptr->n) * ((ptr->prev)->n);
+		ptr->n = muled;
+		_pop(stack, line_number);
+	}
+}
+
+/**
+ * mod - modulus
+ * @stack: list address
+ * @line_number: line
+ *
+ * Return: void
+ */
+
+void _mod(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ptr = *stack;
+	int moded;
+
+	if (counter < 2)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		cleanStack(stack);
+		exit(EXIT_FAILURE);
+	}
+	if (mode == 1)
+	{
+		while (ptr->next != NULL)
+			ptr = ptr->next;
+		if (ptr->n == 0)
+		{
+			fprintf(stderr, "L%d: division by zero\n", line_number);
+			cleanStack(stack);
+			exit(EXIT_FAILURE);
+		}
+		ptr = ptr->prev;
+		moded = ptr->n % (ptr->next)->n;
+		ptr->n = moded;
+		_pop(stack, line_number);
+	}
+	if (mode == 2)
+	{
+		if (ptr->n == 0)
+		{
+			fprintf(stderr, "L%d: division by zero\n", line_number);
+			cleanStack(stack);
+			exit(EXIT_FAILURE);
+		}
+		ptr = ptr->next;
+		moded = ptr->n % (ptr->prev)->n;
+		ptr->n = moded;
+		_pop(stack, line_number);
+	}
+}
+
+/**
+ * pchar - character
+ * @stack: list address
+ * @line_number: command line
+ *
+ * Return: void
+ */
+
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ptr = *stack;
+
+	if (ptr == NULL)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		cleanStack(stack);
+		exit(EXIT_FAILURE);
+	}
+	if (mode == 1)
+	{
+		while (ptr->next != NULL)
+			ptr = ptr->next;
+		putchar(ptr->n);
+	}
+	if (mode == 2)
+		putchar(ptr->n);
+}
+
+/**
+ * pstr - string
+ * @stack: list addrress
+ * @line_number: cmd line
+ *
+ * Return: void
+ */
+
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ptr = *stack;
+
+	if (mode == 1)
+	{
+		while (ptr->next != NULL)
+			ptr = ptr->next;
+		while(ptr->n > 31 && ptr->n < 128)
+		{
+			putchar(ptr->n);
+			ptr = ptr->prev;
+		}
+	}
+	if (mode == 2)
+	{
+		while (ptr->n > 31 && ptr-> < 128)
+		{
+			putchar(ptr->n);
+			ptr = ptr->next;
+		}
+	}
+	putchar('\n');
+}
